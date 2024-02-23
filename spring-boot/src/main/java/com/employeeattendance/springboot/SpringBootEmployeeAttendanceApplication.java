060c6b30a8bfc6cd16f04c5b1ef7d91c70287a98
SantoshAcharya1200 <santosh.acharya1200@gmail.com>
2024-02-23 13:26:34 +0545

    first commit

---
+++ spring-boot/src/main/java/com/employeeattendance/springboot/SpringBootEmployeeAttendanceApplication.java
@@ -0,0 +1,13 @@
+package com.employeeattendance.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEmployeeAttendanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEmployeeAttendanceApplication.class, args);
+	}
+
+}
---
+++ spring-boot/src/test/java/com/employeeattendance/springboot/SpringBootEmployeeAttendanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employeeattendance.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEmployeeAttendanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
